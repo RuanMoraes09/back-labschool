ed7c012dbf4087eb7dfa45f0f872aea8c53b3bf4
Ruan Moraes <ruan.moraes085@gmail.com>
2024-09-03 16:09:59 -0300

    Implements Endpoints of Curso
    Realizado a implementação do endpoints do recurso referente ao curso

--- src/services/CursoService.js
+++ src/services/CursoService.js
@@ -1,9 +1,10 @@
 //importação do database
+const { updateCurso } = require('../controllers/CursoController')
 const database = require('../database')
 
 module.exports = {
 
-    //método para consultar os curso
+    //Consultar os curso
     readCursos: () => {
         return new Promise((resolve, reject) => {
             database.query("SELECT * FROM curso", (err, result) => {
@@ -19,7 +20,46 @@ module.exports = {
     //Cadastrar Curso
     createCurso: (nome) => {
         return new Promise((resolve, reject) => {
-            database.query(`INSERT INTO curso VALUES (null, "${nome}", null)`, (err, result) => {
+            database.query(`INSERT INTO curso VALUES (null, "${nome}", 0)`, (err, result) => {
+                if(err){
+                    reject(err)
+                    return
+                }
+                resolve(result)
+            })
+        })
+    },
+
+    //Pesquisar curso pelo id
+    findCursoById: (id) => {
+        return new Promise((resolve, reject) => {
+            database.query(`SELECT * FROM curso WHERE id = ${id}`, (err, result) => {
+                if(err){
+                    reject(err)
+                    return
+                }
+                resolve(result)
+            })
+        })
+    },
+
+    //Atualizar um curso
+    updateCurso: (id, nome, quantdade) =>{
+        return new Promise((resolve, reject) => {
+            database.query(`UPDATE curso SET nome = "${nome}", quantidade = ${quantidade} WHERE id = ${id}`, (err, result) => {
+                if(err){
+                    reject(err)
+                    return
+                }
+                resolve(result)
+            })
+        })
+    },
+
+    //Deletar um curso
+    deleteCurso: (id) => {
+        return new Promise((resolve, reject) => {
+            database.query(`DELETE FROM curso WHERE id = ${id}`, (err, result) => {
                 if(err){
                     reject(err)
                     return
